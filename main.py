d31d4dd9da7c60e8c6e404daae5d6390c55f831e
smugg99 <dudak799@gmail.com>
2024-04-19 12:43:47 +0200

    test

--- main.py
+++ main.py
@@ -110,6 +110,9 @@ if __name__ == '__main__':
     subprocess.run(['gpio', 'export', str(GPIO_PIN), 'out'])
     time.sleep(1)
     lcd_init()
+    time.sleep(1)
+    lcd_clear()
+    time.sleep(1)
     lcd_thread = threading.Thread(target=update_lcd)
     lcd_thread.daemon = True
     lcd_thread.start()
